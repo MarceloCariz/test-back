38417493ec8ed24e6ad9d1a69eb2fc3e989a6f57
marcelocariz <marcejedd300@hotmail.com>
2023-09-13 13:38:53 -0300

    First commit

--- index.js
+++ index.js
@@ -21,7 +21,25 @@ app.post("/createAccount/", (req, res) => {
     //     account_number: "ES2131313123" 
     // }
     console.log(req.body)
-  res.send(req.body);
+    const account = {
+      name: "test",
+      balance: 2000,
+      accountNumber: "ES2131313123" 
+  }
+res.send(account);
+});
+
+app.patch("/updateBalance/:operation", (req, res) => {
+  // console.log(req.params)
+  const {type , amount} = req.body;
+  const {operation} = req.params;
+
+  const datos = {
+    type,
+    amount,
+    operation
+  }
+res.send(datos);
 });
 
 app.listen(5000, () => {
